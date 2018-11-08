545a6bb259cbb88aa72ad515aa2b4ea159aa71d1
Russ <revoltingrobot@gmail.com>
2018-11-08 14:08:51 +0000

    Added procfile, prepared for heroku

--- app.py
+++ app.py
@@ -72,4 +72,4 @@ def result():
         
 
 
-app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+# app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
